3a25407762c79f660cac4315c8689da5b75db08f
zaidishahbaz <shahbazzaidi15@gmail.com>
2018-11-05 22:08:06 +0530

    Update I Max function

---
+++ I-max_function.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{   long long int k,count=0;
+    long int t,z;
+    cin>>t;
+    z=3;
+    while(t--){
+          while(z--)
+              cin>>k;
+
+        {
+              
+        
+
+
+    
+                if(k%3!=0)
+                {
+        
+            k+=k % 3;
+            
+                }
+        }
+        if(k%3!=0)
+        {
+        cout<<"NO"<<"\n";
+        }
+        else
+        cout<<"YES"<<"\n";
+            
+    
+}
+        
+
+}
